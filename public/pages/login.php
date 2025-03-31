68d08f97946bab6ac35ed7d266b4a13cd617da2d
Domingos Capewa <domingoscapewa08@gmail.com>
2025-03-31 03:01:05 -0300

    Adicionar novas imagens e implementar páginas de login, cadastro e sobre nós

--- public/pages/login.php
+++ public/pages/login.php
@@ -26,7 +26,7 @@ if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../assets/styles/index.css">
+    <link rel="stylesheet" href="../../assets/styles/style.css">
 </head>
 <body>
     <div class="container">
